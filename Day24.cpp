e70d125cdf92281951aebb9233e5393bcfca0ed8
Sanchi Chopra <35348387+SanchiChopra@users.noreply.github.com>
2020-08-24 17:29:42 +0530

    Sum of Left Leaves

---
+++ Day24.cpp
@@ -0,0 +1,39 @@
+/* QUESTION: Find the sum of all left leaves in a given binary tree.
+
+Example:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int left(TreeNode* root){
+        if (!root) return 0;
+        if ((!root->left) && (!root->right))
+            return root->val;
+        return sumOfLeftLeaves(root->right)+left(root->left);
+    }
+
+    int sumOfLeftLeaves(TreeNode* root) {
+        if ((!root) || ((!root->left) && (!root->right)))
+            return 0;  
+        return sumOfLeftLeaves(root->right)+left(root->left);
+    }
+};
